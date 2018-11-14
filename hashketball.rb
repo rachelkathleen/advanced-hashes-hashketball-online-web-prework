e160dcfda5ec82e55a1318758ef3d348107b3f1e
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-14 04:38:37 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -125,13 +125,13 @@ def num_points_scored(player_name)
   end
 end
 
-#def shoe_size(player_name)
-#  game_hash.each do |location, team_data|
-#    if team_data[:players].has_key?(player_name)
-#      return team_data[:players][player_name][:shoe]
-#    end
-#  end
-#end
+def shoe_size(player_name)
+  game_hash.each do |location, team_data|
+    if team_data[:players].has_key?(player_name)
+      return team_data[:players][player_name][:shoe]
+    end
+  end
+end
 
 def team_colors(team_name)
   game_hash.collect do |location, team_data|
